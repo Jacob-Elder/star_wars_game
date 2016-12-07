857a1daceecd836c555484e60ccb8c723b93a442
Jacob-Elder <jacobelderonly@gmail.com>
2016-12-07 15:55:48 -0800

    organized the css into seperate files

---
+++ public/js/endor.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	console.log("welcome to endor!");
+	$('body').css("background-image", "url('/images/endor_background.jpg')");
+	$('.character1').append("<img class='characterPic jawaPic' src='/images/ewok.png'>");
+	$('.character2').append("<img class='characterPic tusken-raiderPic' src='/images/imperial_scout.png'>");
+	$('.character2').css("height", "35%");
+
+
+
+});
